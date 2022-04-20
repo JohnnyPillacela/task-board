1c2241431bfc74bc4dde930281d08cfa6eec3917
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-04-19 23:12:09 -0400

    base page for views

---
+++ src/components/GridView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GridView = props => {
+  return (
+    <div className="page-one">
+      <h1>Page 1: Display By Default</h1>
+    </div>
+  )
+}
+
+export default GridView;
---
+++ src/components/ListView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListView = props => {
+  return (
+    <div className="page-one">
+      <h1>Page 1: Display By Default</h1>
+    </div>
+  )
+}
+
+export default ListView;
