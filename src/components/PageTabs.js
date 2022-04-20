ff834231e49b230994d458f8240acb39cb7139b7
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-04-19 23:11:51 -0400

    page tabs

--- src/components/PageTabs.js
+++ src/components/PageTabs.js
@@ -1,45 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './PageTabs.css';
 
 class PageTabs extends React.Component {
+  state = { currentPage: '/' }
 
-    isActiveTab(tabName) {
+  isActiveTab(tabName) {
+    return (tabName === this.state.currentPage) ? 'nav-link active' : 'nav-link';
+  }
 
-        return (tabName === this.props.currentView) ? 'nav-link active' : 'nav-link';
-    }
+  onTabClick(event, tabName) {
+    this.setState({ currentPage: tabName })
+  }
 
-    onTabClick(event, tabName) {
-        event.preventDefault();
-        this.props.onViewChange(tabName);
-    }
+  render () {
+    return (
+      <ul className='nav page-tabs'>
+        <li className='nav-item'>
+          <Link className={this.isActiveTab('/')} to="/"
+                onClick={event => this.onTabClick(event, '/')}>
+            Grid View
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link className={this.isActiveTab('/list-items')} to="/list-items"
+                onClick={event => this.onTabClick(event, '/list-items')}>
+            List View
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link className={this.isActiveTab('/add-task')} to="/add-task"
+                onClick={event => this.onTabClick(event, '/add-task')}>
+            Add Task
+          </Link>
+        </li>
+      </ul>
+    )
+  }
 
-    render () {
-        return (
-            <div className='nav page-tabs'>
-                <div className='nav-item'>
-                    <a className={this.isActiveTab('page1')}
-                       onClick={(e) => this.onTabClick(e, 'page1')}>
-                        Grid View
-                    </a>
-                </div>
-                <div className='nav-item'>
-                    <a className={this.isActiveTab('page2')}
-                       onClick={(e) => this.onTabClick(e, 'page2')}>
-                        List View
-                    </a>
-                </div>
-                <div className='nav-item'>
-                    <a className={this.isActiveTab('page3')}
-                       onClick={(e) => this.onTabClick(e, 'page3')}>
-                        Add Task
-                    </a>
-                </div>
-            </div>
-
-        )
-    }
-
-}
+};
 
 export default PageTabs;
